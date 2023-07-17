e290d6c585166dce555b8cfe5104e9cf2e859c59
Preston Neal <mrprestonneal@gmail.com>
2023-07-17 08:12:40 -0700

    initial commit

---
+++ Program.cs
@@ -0,0 +1,39 @@
+﻿using System.Text;
+using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+
+var factory = new ConnectionFactory
+{
+    HostName = "localhost",
+    UserName = "guest",
+    Password = "guest",
+    NetworkRecoveryInterval = TimeSpan.FromSeconds(10),
+    AutomaticRecoveryEnabled = true
+};
+using var connection = factory.CreateConnection();
+using var channel = connection.CreateModel();
+
+channel.ExchangeDeclare(exchange: "costing", type: "topic", durable: true);
+
+Dictionary<string, object> rabbitArgs = new Dictionary<string, object>()
+{
+    { "x-queue-type", "quorum" }
+};
+
+channel.QueueDeclare(queue: "costing.test.queue", durable: true, exclusive: false, autoDelete: false, arguments: rabbitArgs);
+
+Console.WriteLine(" [*] Waiting for messages.");
+
+var consumer = new EventingBasicConsumer(channel);
+consumer.Received += (model, ea) =>
+{
+    var body = ea.Body.ToArray();
+    var message = Encoding.UTF8.GetString(body);
+    Console.WriteLine($" [x] Received {message}");
+};
+channel.BasicConsume(queue: "costing.test.queue",
+                     autoAck: true,
+                     consumer: consumer);
+
+Console.WriteLine(" Press [enter] to exit.");
+Console.ReadLine();
